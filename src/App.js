f8752f307d31081ab295a6c21485f7d8a4041ea3
Max Hanson <mthanson2@gmail.com>
2024-01-15 20:50:54 -0800

    website layout and basic css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
+      <section className="side-bar">
+        <button>+ Chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Max</p>
+        </nav>
+      </section>
+      <section className="main"></section>
     </div>
   );
 }
