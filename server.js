3a751fb816821a74cd9060413b51c067bda7e67a
Max Hanson <mthanson2@gmail.com>
2024-01-15 21:53:11 -0800

    successfully connected front end to backend using onclick event on input submit button, api worked succesfully

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(express.json())
-app.use(cors)
+app.use(cors())
 
 
 app.post('/completions', async (req, res) => {
     const options = {
-        method: 'POST',
+        method: "POST",
         headers: {
             "Authorization": `Bearer ${API_KEY}`,
             "Content-Type": "application/json"
@@ -20,6 +20,7 @@ app.post('/completions', async (req, res) => {
         })
     }
     try {
+        console.log("2")
         const response = await fetch('https://api.openai.com/v1/chat/completions', options)
         const data = await response.json()
         res.send(data)
